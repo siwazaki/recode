615cf21fc4f86bdfb492146ca349060c613bb485
iwazaki <s.iwazaki@gmail.com>
2014-03-11 15:39:55 +0900

    test

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -7,9 +7,76 @@ class ApiController extends BaseController {
    *
    * @return Response
    */
-  public function getIndex() {
+  public function getCommits() {
+    //ToDo
+    $userId = 1;
+    $dateFromStr = Input::get('dateFrom');
+    $dateToStr = Input::get('dateTo');
+    $dateFrom = new DateTime($dateFromStr);
+    $dateTo = new DateTime($dateToStr);
+    $query = \Commit::CommitsSummaryBetween($userId, $dateFrom, $dateTo);
+    $commits = $query->get();
 
-    return View::make('dashboard.index');
+    $xkey = "date";
+    $ykeys = array_merge(array_unique(array_pluck($commits, 'repository_name')));
+
+    $d = new \DateTime($dateFromStr);
+
+    $dateToCommit = array();
+    while ($d <= $dateTo) {
+      $date = $d->format('Y-m-d');
+      $one = array('date' => $date);
+      foreach ($ykeys as $ykey) {
+        $one[$ykey] = 0;
+      }
+      $dateToCommit[$date] = $one;
+      $d->add(new DateInterval('P1D'));
+    }
+
+    foreach ($commits as $commit) {
+      $dateToCommit[$commit->date][$commit->repository_name] = $commit->nb_commits;
+    }
+
+    $xs = array();
+    foreach ($dateToCommit as $date => $commits) {
+      $one = array('date' => $date);
+      foreach ($commits as $repository_name => $nbCommits) {
+        $one[$repository_name] = $nbCommits;
+      }
+      $xs[] = $one;
+    }
+
+    return Response::json(
+                    array(
+                        'xkey' => $xkey,
+                        'ykeys' => $ykeys,
+                        'labels' => $ykeys,
+                        'xs' => $xs
+                    )
+    );
+  }
+
+  public function getStats() {
+    $userId = 1;
+    $dateFromStr = Input::get('dateFrom');
+    $dateToStr = Input::get('dateTo');
+    $dateFrom = new DateTime($dateFromStr);
+    $dateTo = new DateTime($dateToStr);
+    $stats = \Stats::getStats($userId, $dateFrom, $dateTo);
+    return Response::json($stats);
+  }
+
+
+  public function postCommits() {
+    $userId = Input::get('userId');
+    $authKey = Input::get('recode_auth_key');
+    //TODO reconcile
+
+    
+
+    var_dump(Input::all());
+    var_dump($userId);
+    var_dump($authKey);
   }
 
 }
