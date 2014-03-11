159e3c8d36679627b700ae84ded775088f1c317e
iwazaki <s.iwazaki@gmail.com>
2014-03-11 15:53:08 +0900

    hoge

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -10,9 +10,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 	public function createApplication()
 	{
 		$unitTesting = true;
-
 		$testEnvironment = 'testing';
-
 		return require __DIR__.'/../../bootstrap/start.php';
 	}
 
