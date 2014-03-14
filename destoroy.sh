b3544f453bb8096b467a96be8b20e4a063e52390
iwazaki <s.iwazaki@gmail.com>
2014-03-15 06:32:50 +0900

    clean

--- destoroy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mysql -u root -ppassw0rd -e"drop database recode"
-mysql -u root -ppassw0rd -e"create database recode"
-composer dump-autoload
-php artisan migrate --seed
-php artisan db:seed --env=local
-
