a485ab9ba3b2bd54e0e3240c74e75f299e8c988f
iwazaki <s.iwazaki@gmail.com>
2014-03-16 23:12:50 +0900

    マージミスを修正

--- public/js/sb-admin.js
+++ public/js/sb-admin.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function() {
 
     $('#side-menu').metisMenu();
@@ -17,23 +16,3 @@ $(function() {
         }
     })
 })
-=======
-$(function() {
-
-    $('#side-menu').metisMenu();
-
-});
-
-//Loads the correct sidebar on window load,
-//collapses the sidebar on window resize.
-$(function() {
-    $(window).bind("load resize", function() {
-        console.log($(this).width())
-        if ($(this).width() < 768) {
-            $('div.sidebar-collapse').addClass('collapse')
-        } else {
-            $('div.sidebar-collapse').removeClass('collapse')
-        }
-    })
-})
->>>>>>> 1a595ceaca8a2e06876f00bc945509c1376dc9fd
